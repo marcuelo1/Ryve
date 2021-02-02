8d9afbd9cbc338538177ba93f6801ea4bc78a0df
Paul Brian Marcuelo <marcuelo1@gmail.com>
2021-02-02 21:24:18 +0800

    Changed host url of production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = {host: "68.183.191.221"}
+  config.action_mailer.default_url_options = {host: "http://68.183.191.221/"}
   config.action_mailer.default_options = {from: "miloemarcuelo@gmail.com"}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
